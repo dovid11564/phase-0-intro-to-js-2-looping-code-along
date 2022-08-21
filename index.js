df83b8982add2f198669f3095650634f52680680
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2022-08-21 09:37:32 -0400

    commit tree e047c0e8225ef629ce0a23aab41b5bd1bfe0a571

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"]
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`)
+    }
+    return gifts
+}
+wrapGifts(gifts)
+const name = ["Ollie", "Samip", "Ali"]
+
+function writeCards(name, event) {
+    for (let i = 0; i < name.length; i++) {
+        name.splice(i, 1, `Thank you, ${name[i]}, for the wonderful surprise gift!`)
+        }
+        return name
+    }
+function countDown(begginingNumber) {
+    // let begginingNumber = x
+    // let begginingNumber = 9
+    while (begginingNumber > 0) {
+        console.log(begginingNumber)
+        begginingNumber -= 1;
+       // x--
+    }
+    //return x
+    console.log(begginingNumber)
+}
+console.log(countDown(-1))
